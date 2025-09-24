dd549ef0f3b082208966984d4769aaf2c6046c15
rbesino238-git <rbesino238@gmail.com>
2025-09-24 19:46:27 +0800

    BasicTheading

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Threading = new System.Windows.Forms.Label();
+            this.StartThread = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Threading
+            // 
+            this.Threading.AutoSize = true;
+            this.Threading.Font = new System.Drawing.Font("Niagara Engraved", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Threading.Location = new System.Drawing.Point(79, 78);
+            this.Threading.Name = "Threading";
+            this.Threading.Size = new System.Drawing.Size(226, 36);
+            this.Threading.TabIndex = 0;
+            this.Threading.Text = "-Before starting thread-";
+            // 
+            // StartThread
+            // 
+            this.StartThread.Font = new System.Drawing.Font("Niagara Engraved", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartThread.Location = new System.Drawing.Point(113, 186);
+            this.StartThread.Name = "StartThread";
+            this.StartThread.Size = new System.Drawing.Size(142, 38);
+            this.StartThread.TabIndex = 1;
+            this.StartThread.Text = "RUN";
+            this.StartThread.UseVisualStyleBackColor = true;
+            this.StartThread.Click += new System.EventHandler(this.StartThread_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(385, 323);
+            this.Controls.Add(this.StartThread);
+            this.Controls.Add(this.Threading);
+            this.ForeColor = System.Drawing.Color.DarkCyan;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Threading;
+        private System.Windows.Forms.Button StartThread;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace BasicThreading
@@ -15,6 +9,47 @@ namespace BasicThreading
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void StartThread_Click(object sender, EventArgs e)
+        {
+            Thread background = new Thread(RunThreads);
+            background.IsBackground = true;
+            background.Start();
+        }
+
+        private void RunThreads()
+        {
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+            Thread ThreadB = new Thread(MyThreadClass.Thread1);
+
+            ThreadA.Name = "ThreadA";
+            ThreadB.Name = "ThreadB";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                Threading.Text = "- End Of Thread -";
+            });
+        }
+    }
+
+    public class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + i);
+                Thread.Sleep(1500);
+            }
         }
     }
 }
